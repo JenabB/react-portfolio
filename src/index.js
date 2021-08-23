4a864fae9561e4390e97a8c5727f5e9af78359b5
JenabB <birulangit1996@gmail.com>
2021-08-23 21:09:29 +0700

    add sw

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
 
 reportWebVitals();
